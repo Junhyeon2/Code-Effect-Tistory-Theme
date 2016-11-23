9764737e13a5b42d26d6deacd96730bdcb8c0349
Jun Hyeon KIM <junhyeon2@naver.com>
2016-11-23 17:40:32 +0900

    article-content까지 완성!

--- WebContent/images/customize.js
+++ WebContent/images/customize.js
@@ -1,8 +1,35 @@
-$(document).ready(function(){ 
-    $('.article-title').css('width', $(window).width()); 
-    $('.article-title').css('height', $(window).height()); 
-    $(window).resize(function() { 
-        $('.article-title').css('width', $(window).width()); 
-        $('.article-title').css('height', $(window).height()); 
-    }); 
+$(document).ready(function() {
+	$('.article-title').css('width', $(window).width());
+	$('.article-title').css('height', $(window).height());
+	$(window).resize(function() {
+		$('.article-title').css('width', $(window).width());
+		$('.article-title').css('height', $(window).height());
+	});
+	$(window).scroll(function() {
+		console.log();
+		if($(document).scrollTop() > $(window).height() - $('#navWrapper').height()){
+			$('#navWrapper').addClass('nav-scroll');
+		}else{
+			$('#navWrapper').removeClass('nav-scroll');
+		}
+	});
+	$('#navLeft').hover(
+		function(){
+			
+			$('#iconBars').addClass('display-none');
+			$('#iconArrow').removeClass('display-none');
+			$('#iconArrow').addClass('rotating');
+
+	    }, function(){
+	    	$('#iconBars').removeClass('display-none');
+			$('#iconArrow').addClass('display-none');
+	    }
+	);
+	$('#navRight').hover(
+			function(){
+				$('#navRight').fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+		    }, function(){
+		    	$('#navRight').fadeIn(100);
+		    }
+		);
 });
